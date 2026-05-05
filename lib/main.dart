17c23aadb62fae1d0407777e7e90cbb7451b9ce0
Mohamed Refai <mohamedrefe1225@gmail.com>
2026-05-06 02:35:19 +0300

    first Virson of calc on calc branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,208 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 // main function is starting point of the app
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          backgroundColor: Colors.indigo[800],
-          title: const Text('Match'),
-        ),
-        body: Match(),
-      ),
-    ),
+    const Calcletor(),
   );
 }
 
-class Match extends StatefulWidget {
-  const Match({super.key});
+class Calcletor extends StatefulWidget {
+  const Calcletor({super.key});
 
   @override
-  State<Match> createState() => _MatchState();
+  State<Calcletor> createState() => _CalcletorState();
 }
 
-class _MatchState extends State<Match> {
-  int leftimage = 1;
-  int rightimage = 2;
-  int trys = 0;
-  void button_1() {
-    leftimage = Random().nextInt(8); //0-7
-    trys++;
-  }
-
-  void button_2() {
-    rightimage = Random().nextInt(8);
-    trys++;
-  }
-
-  void check() {
-    if (leftimage == rightimage) {
-      showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              'You win',
-            ),
-            content: Text('you did it in $trys Tries'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    leftimage = 1;
-                    rightimage = 2;
-                    trys = 0;
-                  });
-                },
-                child: Text('Play Again!'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
+class _CalcletorState extends State<Calcletor> {
+  String displayText = '0';
+  double firstNumber = 0;
+  double secondNumber = 0;
+  String operator = '';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
-          leftimage == rightimage ? 'Victory!' : "Match!",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          'Number of Tries: $trys',
-          style: TextStyle(
-            fontSize: 25,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            side: BorderSide(
+              color: Colors.blueGrey,
+              width: 1,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.black87,
+        body: SafeArea(
+          child: Column(
             children: [
               Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      button_1();
-                      check();
-                    });
-                  },
-                  child: Image.asset('assets/image-$leftimage.png'),
+                flex: 1,
+                child: Container(
+                  color: Colors.white,
+                  alignment: Alignment.bottomRight,
+                  padding: const EdgeInsets.all(24),
+                  child: Text(
+                    displayText,
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
               Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      button_2();
-                      check();
-                    });
-                  },
-                  child: Image.asset('assets/image-$rightimage.png'),
+                flex: 3,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buildButton('7'),
+                          buildButton('8'),
+                          buildButton('9'),
+                          buildButton('/'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buildButton('4'),
+                          buildButton('5'),
+                          buildButton('6'),
+                          buildButton('x'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buildButton('1'),
+                          buildButton('2'),
+                          buildButton('3'),
+                          buildButton('-'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buildButton('0'),
+                          buildButton('.'),
+                          //buildButton('='),
+                          //buildButton('X'),
+                          Expanded(
+                            flex: 1,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromARGB(
+                                  255,
+                                  218,
+                                  185,
+                                  0,
+                                ), // Button background color
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  displayText = '0';
+                                  firstNumber = 0;
+                                  secondNumber = 0;
+                                  operator = '';
+                                });
+                                // Later, we will put the math logic here!
+                                print('pressed');
+                              },
+                              child: Text(
+                                'C',
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromARGB(
+                                  255,
+                                  243,
+                                  135,
+                                  33,
+                                ), // Button background color
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  displayText = '=';
+                                });
+                                // Later, we will put the math logic here!
+                                print(displayText);
+                              },
+                              child: Text(
+                                '=',
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
         ),
-      ],
+      ),
+    );
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0), // Adds a tiny gap between buttons
+        child: OutlinedButton(
+          onPressed: () {
+            setState(() {
+              if (displayText == '0') {
+                displayText = buttonText;
+              } else {
+                displayText = displayText + buttonText;
+              }
+            });
+            // Later, we will put the math logic here!
+            print('$displayText pressed');
+          },
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
